8898ee4242030b8a255905d14c90c66da6ce215c
Vadim Prilepisev <gr.west20@gmail.com>
2025-03-03 15:19:04 +0200

    Device class 0.1

---
+++ src/Device.java
@@ -0,0 +1,56 @@
+import java.util.Objects;
+
+public abstract class Device {
+
+    private final String serialNumber;
+    private String model;
+    private static int totalDevices;
+    private int batteryLevel;
+
+    public Device(String serialNumber, String model, int batteryLevel) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.batteryLevel = batteryLevel;
+        this.totalDevices++;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public static int getTotalDevices() {
+        return totalDevices;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "serialNumber='" + serialNumber + '\'' +
+                ", model='" + model + '\'' +
+                ", batteryLevel=" + batteryLevel +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Device device = (Device) o;
+        return serialNumber.equals(device.serialNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serialNumber);
+    }
+
+}
